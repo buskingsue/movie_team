3f4552bcbbca081594eb762043e9b47606eaf3f3
sgyun96 <tmdrjs78@naver.com>
2025-02-03 15:00:02 +0900

    path 정보

---
+++ job01_crawling.py
@@ -0,0 +1,20 @@
+#poster
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[1]/a/div/div[1]/div[1]/img'
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[2]/a/div/div[1]/div[1]/img'
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[3]/a/div/div[1]/div[1]/img'
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[4]/a/div/div[1]/div[1]/img'
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[5]/a/div/div[1]/div[1]/img'
+'//*[@id="contents"]/div/div/div[3]/div[2]/div[9]/a/div/div[1]/div[1]/img'
+# button 영화/TV 선택 path
+'//*[@id="contents"]/section/div[3]/div/div/div[3]/button'
+#category button 영화 선택 path
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[2]/div[2]/div/button[1]'
+#movie category button  버튼 옆 대괄호 숫자 오른쪽으로 1부터 순서대로 커짐
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[1]'
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[2]'
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[3]'
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[4]'
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[5]'
+'//*[@id="contents"]/section/div[4]/div[2]/div[1]/div[3]/div[3]/div[2]/div/button[17]'
+#필터적용 button
+'//*[@id="contents"]/section/div[4]/div[2]/div[2]/button'
